8aa616df776c2f9d69cf97cd5503af7dc1cb3b0c
Harbey-tech <harbey2965@gmail.com>
2025-06-13 08:16:57 +0100

    for codedeploy

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop the container if it's running
+if [ "$(docker ps -q -f name=simple-python-app)" ]; then
+    echo "Stopping running container: simple-python-app"
+    docker stop simple-python-app
+    docker rm simple-python-app
+else
+    echo "No running container named simple-python-app"
+fi
